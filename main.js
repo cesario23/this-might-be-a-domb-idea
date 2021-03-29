4f9385154fc8e24bb6f852a719d6a7a6789b1421
cesar juarez <cesar.juarez@codeimmersives.com>
2021-03-29 01:33:41 -0700

    code

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+const changeFontColor = document.querySelector ('p');
+changeFontColor.style.color = "blue";
+
+const heading = document.querySelector ("h1");
+heading.style.fontSize = "48px";
+
+const changeText = document.querySelector ('p');
+changeText.innerText = "Hipster Ipsum \: \> Lorem ipsum dolor amet viral meh selfies drinking vinegar\, intelligentsia poke flannel twee paleo enamel pin cray\. Banjo celiac crucifix\, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi\. Tacos hella raclette quinoa blog\, williamsburg adaptogen tbh\. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse\. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag\. Food truck synth wayfarers\, street art banh mi actually authentic\. Bitters tousled tattooed vegan neutra pug hell \ of fixie chia unicorn letterpress \."
+
+const thirteenthElement = document.querySelector ('#item-13');
+thirteenthElement.style.opacity = '.5';
+
+const thirdItem = document.querySelector ('#item-3');
+thirdItem.innerText = "I say, \"Hi!\" ";
+
+const addImage = document.querySelector ('img');
+addImage.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+addImage.style.height = "300px"; 
+addImage.style.width = "300px";
+
+const myImage = document.querySelector ('#img-2');
+myImage.src = "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/03/bok-choy-3-500x500.jpg"
+myImage.style.height = "300px";
+
+const sixteenthItem = document.querySelector ('#item-16')
+sixteenthItem.innerText = "Won't get fooled again."
+
+ 
